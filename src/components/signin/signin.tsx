b0975a5a29cd18338577fb3b94c204241512bca2
melmmmmmmm <melm248@yahoo.co.jp>
2023-05-20 22:07:19 +0900

    signin/up

--- src/components/signin/signin.tsx
+++ src/components/signin/signin.tsx
@@ -10,8 +10,31 @@ const SignIn: React.FC = () => {
         backgroundSize: 'cover',
         backgroundPosition: 'center'
     };
+    const boxStyle: React.CSSProperties = {
+        boxSizing: 'border-box',
+        position: 'absolute',
+        width: '600px',
+        height: '600px',
+        left: '420px',
+        top: '300px',
+        background: 'rgba(205, 201, 195, 0.8)',
+        border: '5px solid #555555',
+        borderRadius: '60px',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+    };
     return (
-        <div style={containerStyle}></div>
+        <div style={containerStyle}>
+            <div style={boxStyle}>
+                <div>Name</div>
+                <input type="text" />
+                <div>Password</div>
+                <input type="text" />
+                <button>Sign in</button>
+            </div>
+        </div>
     );
 };
 
