33d4fdf0986b6edce7e6fe4ecba6c371b8d24a5f
xpa <xpadev@gmail.com>
2023-05-20 14:43:09 +0900

    feat/report-viewer: add edit button

--- src/components/report/viewer/viewer.presenter.tsx
+++ src/components/report/viewer/viewer.presenter.tsx
@@ -2,9 +2,11 @@ import Styles from "./viewer.module.scss";
 import {Card} from "@/components/card/card.presenter";
 import {Report} from "@/@types/report";
 import {Textarea} from "@/components/report/textarea/textarea.presenter";
+import {Edit} from "@/assets/edit";
+import Link from "next/link";
 
 type props = {
-  report: Report&{pinned?: boolean};
+  report: Report&{pinned?: boolean;isMyPost?: boolean};
 }
 
 const ReportViewer = ({report}: props) => {
@@ -13,7 +15,10 @@ const ReportViewer = ({report}: props) => {
       <div className={Styles.header}>
         <Card type={"horizontal"} report={report}/>
       </div>
-      <Textarea content={report.content} readonly/>
+      <div className={Styles.container}>
+        <Textarea content={report.content} readonly/>
+        {report.isMyPost&&<Link href={""} className={Styles.edit}><Edit className={Styles.editIcon} fill={"white"}/></Link>}
+      </div>
     </div>
   )
 }
